48ab8cbb8667b89c8964c1b0055160f5d4fbf06b
cdpn96 <cdpn96@gmail.com>
2018-09-26 12:57:27 -0400

    PHP and HTML Updated

---
+++ rabbitmqphp_example/HTML/Form.php
@@ -0,0 +1,11 @@
+<?php
+include ("Account.php");
+/*MAIN PROGRAM*/
+$db = mysqli_connecti($hostname, $username, $password, $project);
+mysqli_select_db($db, $project);
+
+$user = mysql_real_escape_string($_POST['user']);
+$password = hash('sha256', mysql_real_escape_string($_POST['password']));
+
+$sql = mysql_query("SELECT * FROM XXX_table  WHERE XXX  = '$user' AND XXX = '$password');
+
