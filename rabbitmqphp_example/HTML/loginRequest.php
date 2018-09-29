c5f520b6d99e444d4efc3a5b514deb95e7abbb34
ssjokhla <ssjokhla@gmail.com>
2018-09-29 17:23:50 -0400

    Adding loginRequest.php

---
+++ rabbitmqphp_example/HTML/loginRequest.php
@@ -0,0 +1,35 @@
+<?php
+require_once('/home/samish/IT490/Konsolidate/rabbitmqphp_example/path.inc');
+require_once('/home/samish/IT490/Konsolidate/rabbitmqphp_example/get_host_info.inc');
+require_once('/home/samish/IT490/Konsolidate/rabbitmqphp_example/rabbitMQLib.inc');
+
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+
+
+//$user = $_GET["user"];
+//$password = $_GET["password"];
+
+
+$request = array();
+$request['type'] = "login";
+$request['username'] = $_GET['username'];
+$request['password'] = $_GET['password'];
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+$payload = json_encode($response);
+echo $payload;
+
+
+
+?>
