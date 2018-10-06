b3e598f3d30f034b67bf3ac508a5afec207c0e33
MatthewG218 <mgabriel1460@gmail.com>
2018-10-06 12:18:54 -0400

    Old log server code

---
+++ rabbitmqphp_example/logRabbitMQServer.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","logServer");
+
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
+?>
+
