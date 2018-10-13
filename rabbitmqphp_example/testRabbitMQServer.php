a2cbd52340c9c0111b1305132ec559604045be76
ssjokhla <ssjokhla@gmail.com>
2018-10-13 14:22:46 -0400

    Got testRabbitMQServer.php working with MongoDB, there are other files and I'm not sure yet if they're helping or something we can delete.

--- rabbitmqphp_example/testRabbitMQServer.php
+++ rabbitmqphp_example/testRabbitMQServer.php
@@ -7,31 +7,54 @@ include('myFunctions.php');
 
 function doLogin($username,$password)
 {
-   $con = mysqli_connect("localhost", "admin", "password", "testDB");
+   //$con = mysqli_connect("localhost", "admin", "password", "testDB");
    //$con = mysqli_connect("192.168.0.112", "admin", "password", "testDB");
     //$con = mysqli_connect("192.168.0.108", "admin", "password", "testDB");
-    mysqli_select_db($con, "testDB");
-    $s = "select * from members where username = '$username' and password = '$password'";
+    //mysqli_select_db($con, "testDB");
+    //$s = "select * from members where username = '$username' and password = '$password'";
     //$s = "select * from members where username = '$username' and password = '$password'";
     //echo "SQL Statement: $s";
     //$s = "select * from members where username = 'test' and  password = 'password'";
-    $t = mysqli_query($con, $s);
-    $rowCount = mysqli_num_rows($t);
-    if($rowCount > 0)
+    //$t = mysqli_query($con, $s);
+    //$rowCount = mysqli_num_rows($t);
+    //if($rowCount > 0)
+    //{
+//	    echo "Successful login";
+//	    return "Successful Login";
+//    }
+//    else
+//    {
+//	    echo "Error in logging in";
+	    //Send error to listener
+//	    logError("Authentication Failed when logging in from HTML.");
+	    //error_log("Authentication Failed when logging in from HTML.\n", 3, "/var/log/IT490Logs/master.log");
+//	    return "Bad Login";
+
+//    }
+    // lookup username in databas
+    // check password
+    $connection = new MongoDB\Driver\Manager("mongodb://admin:password@localhost:27017");
+    $filter = array('username'=>$username,'password'=>$password);
+    $options = array('limit'=>1);
+    $query = new MongoDB\Driver\Query($filter, $options);
+    $rows = $connection->executeQuery('TestDB.members', $query);
+    $rowCounter = 0;
+    foreach($rows as $row)
     {
-	    echo "Successful login";
-	    return "Successful Login";
+	$rowCounter = $rowCounter + 1;
+	var_dump($row);
+    }
+    if($rowCounter > 0)
+    {
+	echo "Successful Login.";
+	return "Successful Login";
     }
     else
     {
-	    echo "Error in logging in";
-	    //Send error to listener
-	    logError("Authentication Failed when logging in from HTML.");
-	    //error_log("Authentication Failed when logging in from HTML.\n", 3, "/var/log/IT490Logs/master.log");
-	    return "Bad Login";
+	echo "Error in Logging in";
+	logError("Authentication Failed when logging in from HTML.");
+	return "Bad Login";
     }
-    // lookup username in databas
-    // check password
     return true;
     //return false if not valid
 }
