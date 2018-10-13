d7415a29d5411b9dbc85dfde88d1d9f8b78af015
ssjokhla <ssjokhla@gmail.com>
2018-10-13 15:46:14 -0400

    Transferred functions from testRabbitMQServer.php and logRabbitMQServer.php to myFunctions.php. Tested and made sure everything works with MongoDB

--- rabbitmqphp_example/logRabbitMQServer.php
+++ rabbitmqphp_example/logRabbitMQServer.php
@@ -5,23 +5,6 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 include('myFunctions.php');
 
-    function requestProcessor($request)
-{
-  echo "received request".PHP_EOL;
-  var_dump($request);
-  if(!isset($request['type']))
-  {
-	return "ERROR: unsupported message type";
-  }
-  else if($request['type'] == "log")
-  {
-	$message = "IP_Address: " . $request['IP_ADDR'] . "Message: " . $request['message'] . "\n\n";
-  	error_log($message,3,"Logs/master.log");
-	//return logError($request['IP_ADDR'],$request['message']);
-  }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
-}
-
 $server = new rabbitMQServer("testRabbitMQ.ini","logServer");
 
 echo "logRabbitMQServer BEGIN".PHP_EOL;
