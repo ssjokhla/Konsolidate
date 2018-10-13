a8f528274e283e3f8ee94fc10f45e3ae2f2c58f4
cpacosta <christian.acosta1225@gmail.com>
2018-10-13 15:45:13 -0400

    Commented csvtojson.py

--- jsonConverter/csvtojson.py
+++ jsonConverter/csvtojson.py
@@ -3,9 +3,11 @@
 import csv,json
 import sys
 
+#Opens 'testData.csv' for reading and creates a new file 'jsonFile' for writing
 csvfile = open('testData.csv', 'r')
 jsonfile = open('testData.json', 'w')
 
+#Enter the field names for the data
 fieldnames = ("Time","Par1","Par2","Par3","Level","Date")
 reader = csv.DictReader( csvfile, fieldnames)
 for row in reader:
