712295025bea0e00e42ad5d29609a73e08e45fd1
cpacosta <christian.acosta1225@gmail.com>
2018-10-25 14:39:28 -0400

    touched up rsender

--- rabbitmqphp_example/rsender.sh
+++ rabbitmqphp_example/rsender.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
 
-read -p 'Listerner: ' listener
-read -sp 'Password: ' password
 
+#Ask for listener username and IP
+read -p 'Listerner (user@IP): ' listener
+#read -sp 'Password: ' password
+
+#Set up watcher on /uploads directory to watch for modified, created, or deleted files 
 while inotifywait -r -e modify,create,delete /var/www/html/uploads; do
-	rsync -av --remove-source-files /var/www/html/uploads/ $listener:~/IT490/Konsolidate/rabbitmqphp_example/HTML/test/
+
+	#Sync files to the listeners directory while simultaneously deleting it from local directory
+	rsync -av --remove-source-files /var/www/html/uploads/ $listener:~/IT490/Konsolidate/rabbitmqphp_example/RSync/
+
 done
-
-
