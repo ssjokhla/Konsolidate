0d285fa8c2e1faef6b858cf4037915ac78160c0f
cdpn96 <cdpn96@gmail.com>
2018-10-27 12:50:31 -0400

    test123

--- rabbitmqphp_example/myFunctions.php
+++ rabbitmqphp_example/myFunctions.php
@@ -3,7 +3,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 //$test = $_SERVER['REMOTE_ADDR'];
-
+//Danh was here.
 
 function logError($message)
 {
