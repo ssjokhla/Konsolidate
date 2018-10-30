3ea5dceee99c2d6f9a814edb6574abd35ed9eff2
ssjokhla <ssjokhla@gmail.com>
2018-10-29 21:57:55 -0400

    Changed paths

--- rabbitmqphp_example/listeners/logRabbitMQServer.php
+++ rabbitmqphp_example/listeners/logRabbitMQServer.php
@@ -1,8 +1,8 @@
 #!/usr/bin/php
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
+require_once('../path.inc');
+require_once('../get_host_info.inc');
+require_once('../rabbitMQLib.inc');
 //include('myFunctions.php');
 
 function requestProcessor($request)
--- rabbitmqphp_example/listeners/testRabbitMQServer.php
+++ rabbitmqphp_example/listeners/testRabbitMQServer.php
@@ -1,9 +1,9 @@
 #!/usr/bin/php
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
-include('myFunctions.php');
+require_once('../path.inc');
+require_once('../get_host_info.inc');
+require_once('../rabbitMQLib.inc');
+include('../myFunctions.php');
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 echo "testRabbitMQServer BEGIN".PHP_EOL;
