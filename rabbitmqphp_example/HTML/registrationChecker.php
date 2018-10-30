b4837e517a16d6f6d90d2f5ded3b635879d190eb
cpacosta <christian.acosta1225@gmail.com>
2018-10-29 20:26:28 -0400

    Changed registrationChecker.php

--- rabbitmqphp_example/HTML/registrationChecker.php
+++ rabbitmqphp_example/HTML/registrationChecker.php
@@ -1,33 +1,37 @@
 <?php
 include('/home/chris/490project/Konsolidate/rabbitmqphp_example/myFunctions.php');
-	if(isset($_GET['Register']))
+
+$client = new rabbitMQClient("testRabbitMQ.ini","registerServer");
+if(isset($_GET['Register']))
+{
+	$request = array();
+	$request['type'] = "register";
+	$request['username'] = $_GET['username'];
+	$request['password'] = $_GET['password'];
+	$request['role'] = $_GET['role'];
+	$request['message'] = $msg;
+	$response = $client->send_request($request);
+
+	echo "client received response: ".PHP_EOL;
+	print_r($response);
+
+
+	echo "\n\n";
+
+	echo $argv[0]." END".PHP_EOL;
+
+
+	$payload = json_encode($response);
+	echo $payload;
+	if($payload==true)
 	{
-		$success = 0;
-		$username=$_GET['username'];
-		$password=$_GET['password'];
-		$role=$_GET['role'];
-		$con = mysqli_connect("localhost", "admin", "password", "masterDB");
-		mysqli_select_db($con, "masterDB");
-		$s = "select * from members where username = '$username'";
-	//	echo "SQL Statement: $s";
-		$t = mysqli_query($con, $s);
-		$rowCount = mysqli_num_rows($t);
-		if($rowCount > 0)
-		{
-		  	echo "Please Choose another username";
-		  	logError("Username already in database");
-			//return "Error";
-			pageLoader("registration.php");
-		}
-		else
-		{
-			$success = 1;
-			$r = "Insert into members (username, password, role)
-			Values('$username', SHA2('$password',512),'$role')";
-			$tr= mysqli_query($con,$r);
-			//return "Complete";
-			echo "Sucessfully created Account! :>";
-			pageLoader("login.html");
-		}
+		pageLoader("login.html");
 	}
+	else
+	{
+		pageLoader("registration.html");
+	}
+
+
+}
 ?>
