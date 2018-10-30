970bf08d87fc3d7a9622dfb9ca594c391bf80f90
ssjokhla <ssjokhla@gmail.com>
2018-10-29 20:51:30 -0400

    Removed requestProcessor from regRabbitMQServer.php

--- rabbitmqphp_example/regRabbitMQServer.php
+++ rabbitmqphp_example/regRabbitMQServer.php
@@ -5,23 +5,6 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 include('myFunctions.php');
 
-function requestProcessor($request)
-{
-	echo "received request".PHP_EOL;
-	var_dump($request);
-	if(!isset($request['type']))
-	{
-		return "ERROR: unsupported message type";
-	}
-	switch ($request['type'])
-	{
-	case "reg":
-		return doRegister($request['username'],$request['password'],$request['role']);
-	}
-	return array("returnCode" => '0', 'message'=>"Server received request and processed");
-}
-
-
 $server = new rabbitMQServer("testRabbitMQ.ini","registerServer");
 
 echo "regRabbitMQServer BEGIN".PHP_EOL;
