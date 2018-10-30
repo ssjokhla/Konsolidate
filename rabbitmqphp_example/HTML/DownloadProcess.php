479f60e314e4c8a154613a3b03b0dfa23d4f5b09
ssjokhla <ssjokhla@gmail.com>
2018-10-30 18:32:55 -0400

    Working on downloads

--- rabbitmqphp_example/HTML/DownloadProcess.php
+++ rabbitmqphp_example/HTML/DownloadProcess.php
@@ -1,36 +1,35 @@
 <?php
+include('/home/cong-danh/IT490/Konsolidate/rabbitmqphp_example/myFunctions.php');
+session_start();
+
 $client = new rabbitMQClient("testRabbitMQ.ini","registerServer");
 if(isset($_GET['download']))
 {
-        $request = array();
-        $request['type'] = "reg";
-        $request['username'] = $_GET['username'];
-        $request['password'] = $_GET['password'];
-        $request['role'] = $_GET['role'];
-        $request['message'] = $msg;
-        $response = $client->send_request($request);
-
-        echo "client received response: ".PHP_EOL;
-        print_r($response);
-
-
-        echo "\n\n";
-
-        echo $argv[0]." END".PHP_EOL;
-
-
-        $payload = json_encode($response);
-        echo $payload;
-        if($payload==true)
-        {
-                pageLoader("login.html");
-        }
-        else
-        {
-                pageLoader("registration.html");
-                logError("Failed registration because username already exists");
-        }
-
-
-}
+	$request = array();
+	$request['type'] = "down";
+	$request['message'] = $msg;
+	$response = $client->send_request($request);
+	$response = $client -> send_request($request);
+	$files = scandir("/var/www/html/downloads/");
+	echo "$files[2]";
+/*
+	for($i=2; $i < count($files); $i++)
+	{
 ?>
+<!DOCTYPE>
+<html>
+<p>
+	<a href="downloads/
+<?php
+		echo $files[$i];
+			
+?>
+"><?php echo $files[$i]; ?></a>
+			</p>
+<?php
+			echo count($files);
+		echo "<br> count of uploads folder is:".count($files1);
+	}
+ */
+?>
+</html>
