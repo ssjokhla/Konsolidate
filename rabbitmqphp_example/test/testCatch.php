f687eed591e10b0657d4068b30d0ba9988be8c24
cpacosta <christian.acosta1225@gmail.com>
2018-10-31 21:25:10 -0400

    pushing testCatch

--- rabbitmqphp_example/test/testCatch.php
+++ rabbitmqphp_example/test/testCatch.php
@@ -8,28 +8,19 @@ function doConnect()
 {
 	$hostname_db = "localhost";
 	$database_db = "masterDB";
-	$username_db = "admin";
+	$username_db = "amin";
 	$password_db = "password";
 
-	try{
+	$db = mysqli_connect($hostname_db, $username_db, $password_db, $database_db);
 
-		if($db = mysqli_connect($hostname_db, $username_db, $password_db, $database_db))
-		{
-			echo "Database Connected";
-			mysqli_query($db, $database_db);
-			$s = "select * from members";
-			$t = mysqli_query($con, $s);
-			echo $t;
-		}
-		else
-		{
-			throw new Exception("Unable to connect to database");
-		}
+	mysqli_select_db($db, $database_db);
+
+	if(!$db){
+		logError("Connection Failed: " . mysqli_connect_error());
+		die("Connection Failed: " . mysqli_connect_error());
 	}
-	catch(Exception $e)
-	{
-		logError($e->getMessage());
-	}
+
 }
+doConnect();
 ?>
 
