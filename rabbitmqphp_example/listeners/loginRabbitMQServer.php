1e467e74f248444c5d0edd46847de1019842facc
ssjokhla <ssjokhla@gmail.com>
2018-11-08 15:48:02 -0500

    Changed testRabbitMQServer.php to be loginRabbitMQServer.php

--- rabbitmqphp_example/listeners/loginRabbitMQServer.php
+++ rabbitmqphp_example/listeners/loginRabbitMQServer.php
@@ -5,8 +5,8 @@ require_once('../get_host_info.inc');
 require_once('../rabbitMQLib.inc');
 include('../myFunctions.php');
 
-$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
-echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server = new rabbitMQServer("testRabbitMQ.ini","loginServer");
+echo "loginRabbitMQServer BEGIN".PHP_EOL;
 $server->process_requests('requestProcessor');
-echo "testRabbitMQServer END".PHP_EOL;
+echo "loginRabbitMQServer END".PHP_EOL;
 exit();
