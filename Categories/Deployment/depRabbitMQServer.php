943f55779fb298adcbde395fe3b57bcc00c4c85c
ssjokhla <ssjokhla@gmail.com>
2018-11-24 13:30:07 -0500

    Changing some paths for new file structure

---
+++ Categories/Deployment/depRabbitMQServer.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+require_once('~/Konsolidate/Categories/Require/path.inc');
+require_once('~/Konsolidate/Categories/Require/get_host_info.inc');
+require_once('~/Konsolidate/Categories/Require/rabbitMQLib.inc');
+
+$server = new rabbitMQServer("testRabbitMQ.ini","depServer");
+echo "depRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "depRabbitMQServer END".PHP_EOL;
+exit();
