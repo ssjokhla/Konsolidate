3d3cfb9c59f458d5765b0c765b0003d795735b52
cpacosta <christian.acosta1225@gmail.com>
2018-11-24 13:48:57 -0500

    Changed file paths for Parser

--- Categories/Parsing/testParser2.php
+++ Categories/Parsing/testParser2.php
@@ -4,7 +4,7 @@ $file = $argv[1];
 $con = mysqli_connect("localhost", "admin", "password", "masterDB");
 mysqli_select_db($con, "masterDB");
 
-$query = "LOAD DATA LOCAL INFILE '~/Konsolidate/Categories/RSync/$file'
+$query = "LOAD DATA LOCAL INFILE '~/Konsolidate/Categories/RSync/FileTransfer/$file'
 	INTO TABLE dataTable 
 	FIELDS TERMINATED BY ',' 
 	LINES TERMINATED BY '\n'";
