0be7a812799f5480ce31954b30e56253d7bb874f
cdpn96 <cdpn96@gmail.com>
2018-11-26 18:23:17 -0500

    Got loginRabbitMQServer.php running within Konsolidate directory

--- newKonsolidate/Categories/Sessions/loginRabbitMQServer.php
+++ newKonsolidate/Categories/Sessions/loginRabbitMQServer.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?php
-require_once('~/Konsolidate/Categories/Require/path.inc');
-require_once('~/Konsolidate/Categories/Require/get_host_info.inc');
-require_once('~/Konsolidate/Categories/Require/rabbitMQLib.inc');
-include('~/Konsolidate/Categories/Require/RequestProcessorFunctions.php');
+require_once('../Require/path.inc');
+require_once('../Require/get_host_info.inc');
+require_once('../Require/rabbitMQLib.inc');
+include('../Require/RequestProcessorFunctions.php');
 
-$server = new rabbitMQServer("~/Konsolidate/Categories/Require/testRabbitMQ.ini","loginServer");
+$server = new rabbitMQServer("testRabbitMQ.ini","loginServer");
 echo "loginRabbitMQServer BEGIN".PHP_EOL;
 $server->process_requests('requestProcessor');
 echo "loginRabbitMQServer END".PHP_EOL;
