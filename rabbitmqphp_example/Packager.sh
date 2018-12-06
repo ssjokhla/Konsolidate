db53e5a6fe2ca6ec6c8b592b4d7155a5060d080a
cpacosta <christian.acosta1225@gmail.com>
2018-12-06 14:04:22 -0500

    Changing Version control to deployment

--- rabbitmqphp_example/Packager.sh
+++ rabbitmqphp_example/Packager.sh
@@ -2,17 +2,13 @@
 
 printf "Please enter the category you want to package: "
 read -r category
-printf "Please enter the version number: "
-read -r version
-#printf "Please enter a description of the package: "
-#read -r description
 
 echo $category , $version #$description
 
-tar -czvf /var/Konsolidate/Pending/$category\_$version.tar.gz /var/Konsolidate/Categories/$category
+tar -czvf /var/Konsolidate/Pending/$category.tar.gz /var/Konsolidate/Categories/$category
 
 echo "Packager.sh if statement ran"
 
-php ~/IT490/Konsolidate/rabbitmqphp_example/devPackage.php $category $version #$description
+php ~/IT490/Konsolidate/rabbitmqphp_example/devPackage.php $category
 
 #devPackage($name, $version, $path, $status, $description)
