77e28c4e5f6fce6ea954f3bc7f89ad5501dd3045
ssjokhla <ssjokhla@gmail.com>
2018-12-15 14:03:33 -0500

    Added inotifywait to change IP addresses in testRabbitMQ.ini file

--- Categories/Failover/changer.sh
+++ Categories/Failover/changer.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-sed -i "/.*BROKER_HOST =.*/c\BROKER_HOST = $IP" /var/Konsolidate/Categories/Require/testRabbitMQ.ini          # This will be used by all other VMs to allow for connection
+
+inotifywait -r -e modify,create --format '%f' /var/Konsolidate/Categories/Failover/ChangeFile | while read FILE
+do
+
+        echo "File is named $FILE"
+
+	IP=`cat $FILE`
+	sed -i "/.*BROKER_HOST =.*/c\BROKER_HOST = $IP" /var/Konsolidate/Categories/Require/testRabbitMQ.ini          # This will be used by all other VMs to allow for connection
+
+        echo "Yay this worked"
+
+done
 
