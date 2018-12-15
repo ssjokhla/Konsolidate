d5865e044a481a6af3fcbca40f20a8bcf80d458f
cdpn96 <cdpn96@gmail.com>
2018-12-15 17:23:50 -0500

    Fixed repeat of changer

--- Categories/Failover/pinger.sh
+++ Categories/Failover/pinger.sh
@@ -2,7 +2,7 @@
 
 #sudo watch -n 1 ~/Konsolidate/Categories/Failover/pinger.sh # (This will run the script every second)
 
-ping -c1 192.168.0.108 > /dev/null              #The "c" represents how many pings to send.
+ping -c1 192.168.0.107 > /dev/null              #The "c" represents how many pings to send.
 
 if [ $? -eq 0 ]; then                           #If the ping went through, zero will result.
 {
@@ -10,11 +10,8 @@ if [ $? -eq 0 ]; then                           #If the ping went through, zero 
 }
 else
 {
-        echo "Currently not working: $(date)"
+	echo "Currently not working: $(date)"
         hostname -I > /var/Konsolidate/Categories/Failover/backEndIP.txt
-        #IP=$(hostname -I)                      #Grabs the IP of this system.
-
-        scp backEndIP.txt qa@192.168.0.108:/var/Konsolidate/Categories/Failover/ChangeFile	
+        scp backEndIP.txt qa@192.168.0.108:/var/Konsolidate/Categories/Failover/ChangeFile
 }
 fi
-
